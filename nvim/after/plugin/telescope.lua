430229e29752d6186966077df7815e955124e96f
Lars Hampe <hello@hashdot.co>
2023-01-12 17:08:14 +0100

    add new keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,25 @@
+local status, telescope = pcall(require, 'telescope')
+if (not status) then return end
 local builtin = require('telescope.builtin')
+
+-- show buffers
+vim.keymap.set('n', '\\\\', function()
+  builtin.buffers()
+end)
+
+vim.keymap.set('n', 'sf', function()
+  telescope.extensions.file_browser.file_browser({
+    path = "%:p:h",
+    cwd = telescope_buffer_dir(),
+    respect_gitignore = false,
+    hidden = true,
+    grouped = true,
+    previewer = false,
+    initial_mode = "normal",
+    layout_config = { height = 40 }
+  })
+end)
+
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<C-s>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ss', function()
--- nvim/lua/hashdot/remap.lua
+++ nvim/lua/hashdot/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
+
+-- Open file browser
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move marked text up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -13,7 +16,22 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- Yank to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- Replace current selected text
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Split Window
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move Window
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sl', '<C-w>l')
+
+-- Select all
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
